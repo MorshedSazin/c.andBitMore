caa35876afbb4cafb040e6d4b423daf5186b0b48
morshedsazin <morshedsazin@gmail.com>
2023-05-16 13:57:12 +0600

    all files are added

--- main.c
+++ main.c
@@ -1,36 +1,11 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
  int  main(){
-	// int num1; int num2;
-	// scanf("%d", &age);
-	// int favNum = 90;
-
-	// char color[20];
-	// char pluralNoun[20];
-	// char celebrityF[20],celebrityL[20];
-
-	char name[20];
-
-	// printf("Enter a color: ");
-	// scanf("%s", color);
-	// printf("Enter a pluralNoun :");
-	// scanf("%s", pluralNoun);
-	// printf("Enter a celebrity name :");
-	// scanf("%s%s", celebrityF,celebrityL);
-	///--------------------------------
-	printf("enter something for fgets: ");
-	fgets(name, 20, stdin);
-
-
-	// printf("rose are %s\n",color);
-	// printf("%s are blue\n",pluralNoun);
-	// printf("I love %s %s\n",celebrityF,celebrityL );
-
-	printf("Your name is %s abcdefg\n", name);
-			
+		 int list [] = {4,5,6,7,8,9};
+		 list [1] = 200;
+		 printf("%d\n", list[1]);
  return 0;
 }
  
